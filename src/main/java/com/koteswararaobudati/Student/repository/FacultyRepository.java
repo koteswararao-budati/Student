b5acb035af5cbc1547b6b43e762b40fd108dc8b1
koteswararao-budati <koteswararaob94@outlook.com>
2023-11-01 22:54:19 -0500

    Updated project as per the guidelines and used lombok to reduce boilerplate code Signed-off-by: koti <koteswararaob94@outlook.com>

--- src/main/java/com/koteswararaobudati/Student/repository/FacultyRepository.java
+++ src/main/java/com/koteswararaobudati/Student/repository/FacultyRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 
 @Repository
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    //The Optional transform into the below query in sql
 //    @Query("SELECT f FROM Faculty f WHERE f.email=?1")
     Optional<Faculty> findFaultyByEmail(String email);
 }
